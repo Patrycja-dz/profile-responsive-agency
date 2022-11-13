165981c2ee08a95ab695ece7d3c8a76aa1d3ce60
Patrycja Dziemianko <patrycja.dziemianko@eclipsegroup.co.uk>
2022-11-13 12:28:32 +0100

    added basic slider to the page

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+    slidesToShow: 4,
+    adaptiveHeight: false,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+  });
--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 // import './style/sass/index.scss'
-import A11YSlider from "a11y-slider";
-const slider = new A11YSlider(document.querySelector('.slider'), {
-    adaptiveHeight: true,
-    dots: false
-  });
+
