c0fcfa770c192acb22e15394a741200527765ddb
Patrycja Dziemianko <patrycja.dziemianko@eclipsegroup.co.uk>
2022-11-13 14:01:16 +0100

    styling slider

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-
 const slider = new A11YSlider(document.querySelector(".slider"), {
-    slidesToShow: 4,
-    adaptiveHeight: false,
-    dots: true,
-    centerMode: true,
-    arrows: false,
-  });
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  infinite: true,
+  responsive: {
+    576: {
+      slidesToShow: 2,
+    },
+    760: {
+    //   arrows: true,
+      slidesToShow: 4,
+    },
+  },
+});
+
